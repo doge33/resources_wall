16825f44b2fbce2cf85ce9535102e1592703d8d1
Sandy Long <sandylx33@gmail.com>
2021-02-09 23:41:57 -0500

    changed bcrypt back(only in User.js); changed sslmode to disable in resetdb; added ssl setting to dbParams

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const bodyParser = require("body-parser");
 const sass = require("node-sass-middleware");
 const app = express();
 const morgan = require('morgan');
-const bcrypt = require('bcrypt');
+
 //const cookieSession = require("cookie-session")
 const session = require("cookie-session")
 const connectFlash = require("connect-flash");

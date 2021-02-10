7712e7b899160b5ff900fd88d69d00cfd08bfcff
doge33 <sandylx33@gmail.com>
2021-02-10 16:50:03 +0000

    commented our local connectionString in resetdb.js

--- bin/resetdb.js
+++ bin/resetdb.js
@@ -5,13 +5,13 @@
 // other dependencies
 const fs = require('fs');
 const chalk = require('chalk');
-const Client = require('pg');
+const Client = require('pg-native');
 const app = express();
 if (app.get('env') == 'development'){ require('dotenv').config()};
 
 // PG connection setup
-const connectionString = process.env.DATABASE_URL ||
-  `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
+const connectionString = process.env.DATABASE_URL //||
+  //`postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
 const client = new Client();
 
 // Loads the schema files from db/schema
